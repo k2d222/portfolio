2d92b5ae6a5085e106d3e351d1e9ca8954118e4b
mathis <mathis.brossier@universite-paris-saclay.fr>
2021-11-03 20:16:30 +0100

    nice project banners

--- ts/Project.vue
+++ ts/Project.vue
@@ -1,13 +1,52 @@
 <style lang="sass">
+  .project-overview
+    overflow: hidden
+    margin: 3rem
+    margin-bottom: 0
+    padding: 1rem
+    display: flex
+    justify-content: space-between
+    flex-wrap: wrap
+    position: relative
+
+    img
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      object-fit: cover
+      z-index: -1
+
+    h1
+      margin-top: 0
+
+    h3 + *
+      margin-left: 1rem
+
+    .right
+      padding: 1rem
+      background: rgba(0, 0, 0, .9)
+      &:empty
+        display: none
+
+      > *:first-child h3
+        margin-top: 0
+
+    .left
+      h1, h2
+        & > span
+          background: rgba(0, 0, 0, .9)
+          padding: .5rem 1rem
+
   .project
     font-size: 1.5rem
     font-family: sans-serif
-    margin: 3rem 3rem
+    margin: 3rem
+    margin-top: 0
     background: #dedede
     color: #171617
     padding: 2rem
-    border-radius: 5px
-    box-shadow: 0px 0px 0px 2px black
 
     > p:first-child
       margin-top: 0
@@ -23,19 +62,6 @@
       font-size: 1rem
       margin-left: 2rem
       margin-bottom: 2rem
-
-  .project-overview
-    overflow: hidden
-    margin: 5rem 5rem
-    display: flex
-    justify-content: space-between
-    flex-wrap: wrap
-
-    h1
-      margin-top: 0
-
-    h3 + *
-      margin-left: 1rem
 </style>
 
 <template>
@@ -43,8 +69,8 @@
     <section class="project-overview">
       <img :src="getImg()" />
       <div class="left">
-        <h1>{{ proj("name") }}</h1>
-        <h2 class="brief">{{ proj("brief") }}</h2>
+        <h1><span>{{ proj("name") }}</span></h1>
+        <h2 class="brief"><span>{{ proj("brief") }}</span></h2>
       </div>
       <div class="right">
         <div v-if="proj_e('collab')">
